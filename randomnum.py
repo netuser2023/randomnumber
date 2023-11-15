0173436be362c470b53cd78d6c2fe9f609ff218b
netuser2023 <wittechuz@gmail.com>
2023-11-15 19:48:42 +0500

    version 1.1

--- randomnum.py
+++ randomnum.py
@@ -2,10 +2,21 @@ import random
 import time
 
 
+randomnum = input("Please choise the random number (10/20/30/40/50): ") # --- new version
 print("Generating random number for you!! ")
 print("Please wait! ")
 time.sleep(3)
-print(random.randrange(1, 50))
+
+if randomnum == "10": 
+    print(random.randrange(1, 10)) # ---- new version
+elif randomnum == "20":
+    print(random.randrange(10, 20))
+elif randomnum == "30":
+    print(random.randrange(20, 30))
+elif randomnum == "40":
+    print(random.randrange(30, 40))
+elif randomnum == "50":
+    print(random.randrange(40, 50))
 
 print("Do you like the number??")
 a = input(" --> (yes/no): ")
